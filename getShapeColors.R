a077cd4d4e0d2d8b60776a1e77d796271a5bfdd5
Sophie Sommer <sjs908@nyu.edu>
2020-04-25 14:59:17 -0400

    update to allow changing max opacity

--- getShapeColors.R
+++ getShapeColors.R
@@ -1,4 +1,4 @@
-getShapeColors <- function(data, nbreaks=5, missingcol="gray88", rdgrbl=c(0,0,1), mycolors=NULL){
+getShapeColors <- function(data, nbreaks=5, missingcol="gray88", rdgrbl=c(0,0,1), mycolors=NULL, max_opac=0.55){
   if(length(nbreaks) == 1) {
     mybreaks <- c(-1, quantile(data,
                                probs = seq(0, 1, length.out=nbreaks+1),
@@ -10,7 +10,7 @@ getShapeColors <- function(data, nbreaks=5, missingcol="gray88", rdgrbl=c(0,0,1)
     mycols <- mycolors
   } else {
     mycols <- rgb(rdgrbl[1],rdgrbl[2],rdgrbl[3],
-                  seq(0.05,0.55, length.out = length(mybreaks)-1))
+                  seq(0.05,max_opac, length.out = length(mybreaks)-1))
   }
   mycols[1] <- missingcol
   data[is.na(data)] <- -.1
